eaefbb72bf85f5b0808cb6709a2aabe205752173
Aaron Henshaw <aaron@ambush.capital>
2025-01-24 20:18:30 -0500

    remove some comments, test verified commits

--- src/main.rs
+++ src/main.rs
@@ -30,49 +30,6 @@ fn main() {
         CommitmentConfig::confirmed(),
     );
 
-    // let program = client
-    //     .program(Pubkey::from_str("dRiftyHA39MWEi3m9aunc5MzRF1JYuBsbn6VPcn33UH").unwrap())
-    //     .unwrap();
-
-    // let filters = vec![RpcFilterType::Memcmp(Memcmp::new_raw_bytes(
-    //     0,
-    //     [100, 177, 8, 107, 168, 65, 65, 39].to_vec(),
-    // ))];
-
-    // let account_config = RpcAccountInfoConfig {
-    //     commitment: Some(program.rpc().commitment()),
-    //     encoding: Some(UiAccountEncoding::Base64Zstd),
-    //     ..RpcAccountInfoConfig::default()
-    // };
-
-    // let gpa_config = RpcProgramAccountsConfig {
-    //     filters: Some(filters),
-    //     account_config: account_config.clone(),
-    //     with_context: Some(true),
-    // };
-
-    // let accounts = program.rpc().get_program_accounts_with_config(&program.id(), gpa_config);
-
-    // match accounts {
-    //     Ok(spot_market_accounts) => {
-    //         for (pubkey, account) in spot_market_accounts {
-    //             match SpotMarket::try_deserialize(&mut &account.data[..]) {
-    //                 Ok(spot_market) => {
-    //                     println!(
-    //                         "Spot Market {} - Name: {:?}, Pubkey Length: {}, Market Index: {}",
-    //                         pubkey,
-    //                         pubkey.to_string().len(),
-    //                         String::from_utf8_lossy(&spot_market.name).trim_matches(char::from(0)),
-    //                         spot_market.market_index
-    //                     );
-    //                 }
-    //                 Err(e) => println!("Failed to deserialize spot market {}: {}", pubkey, e),
-    //             }
-    //         }
-    //     }
-    //     Err(e) => println!("Failed to fetch accounts: {}", e),
-    // }
-
     let kamino_client = KaminoClient::new(&client);
 
     match kamino_client.get_obligations("AmrekAq6s3n2frDi67WUaZnbPkBb1h4xaid1Y8QLMAYN") {
